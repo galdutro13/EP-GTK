915db7a3c5b447abf3cce105e686e0f13a766dfb
gregassagraf <gregassagraf@usp.br>
2022-06-29 00:51:00 -0300

    Initial GTK implementation

---
+++ src/GTK_MenuBar.rs
@@ -0,0 +1,4 @@
+
+
+
+//fn build_menubar()
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use mysql::*;
 use mysql::prelude::*;
 
 use gtk::prelude::*;
-use gtk::{Window, WindowType, Label};
 
+use gtk::{Application, ApplicationWindow, Button, MenuBar, MenuButton};
+use gtk::ffi::GtkApplication;
 fn main() {
 
     println!("Hello, world!");
@@ -14,7 +15,7 @@ fn main() {
         panic!("Can't init GTK!");
     }
 
-    let window = Window::new(WindowType::Toplevel);
+    /*let window = Window::new(WindowType::Toplevel);
     let label = Label::new(Some("Some text"));
 
 
@@ -23,8 +24,36 @@ fn main() {
     window.set_default_size(350, 120);
 
     window.add(&label);
-    window.show_all();
-    gtk::main();
+    window.show_all();*/
+
+    let sgbd_museu = Application::builder().application_id("SGBD Museu").build();
+    let menu_museu =
+    sgbd_museu.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("Sistema Unificado de Arquiologia")
+            .default_width(540)
+            .default_height(360)
+            .build();
+
+
+        let button = Button::with_label("Hey!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+
+
+        window.add(&button);
+        window.add(&menu_bar);
+        window.show_all();
+
+
+
+    });
+
+    sgbd_museu.run();
+
+    //gtk::main();
 
 
 }
