98b3c24bb8c4bae6deaf01eb45ffdb3fa48f4da8
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-07-21 04:54:32 -0300

    Mudanças de estrutura

--- Crud/Client/museu/src/App.js
+++ Crud/Client/museu/src/App.js
@@ -1,37 +1,16 @@
 import React, {useState} from "react"
 import './App.css';
 import Header from "./componentes/Header";
- 
+import Body from "./componentes/Body"; 
+import Footer from "./componentes/Footer";
+
 function App() {
-  const [values, setValues] = useState();
-  console.log(values);
-  const handleChangeValues = (value) =>{
-    setValues(prevValue=>({
-      ...prevValue, 
-      [value.target.name]: value.target.value, 
-    }));
-  };
 
-  const handleClickButton = () =>{
-    console.log(values);
-  };
-
-  return (
-    <div className="app--container">
-      <div className='register--container'>
-      <h1 className='register--title'>
-        Museu Nustagem
-      </h1>
-      <input type = "text" name = "name" placeholder = "Nome" className="register--input" onChange={handleChangeValues}/>
-      <input type = "text" name = "cost" placeholder = "Preço" className="register--input" onChange={handleChangeValues}/>
-      <input type = "text" name = "category" placeholder = "Categoria" className="register--input" onChange={handleChangeValues}/>
-      <button className='register--button' onClick={() => handleClickButton()}> Cadastrar Objeto</button>
-    </div>
-    </div>
-  );
   return(
     <>
       <Header/>
+      <Body />
+      <Footer />
     </>
   )
 }
--- Crud/Client/museu/src/index.js
+++ Crud/Client/museu/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //estou chamando o componente root(app para aparece no index)
 root.render(
