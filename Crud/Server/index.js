888c6f7e4db9e883b7c61410d08b7a62a453fc19
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-07-24 04:44:49 -0300

    Select dinâmico e add select

--- Crud/Server/index.js
+++ Crud/Server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 //Iniciando servidor
+const cors = require("cors");
 
 const db = mysql.createPool({
     host: "localhost",
@@ -10,16 +11,32 @@ const db = mysql.createPool({
     database: "crudgames",
 })
 
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) =>{
-    let SQL = "INSERT INTO games ( name, cost, category) VALUES ( 'teste', '120', 'ação')"
-    db.query(SQL, (err, result)=>{
+app.post("/register", (req, res)=>{
+    const {name} = req.body;
+    const {cost} = req.body;
+    const {category} = req.body;
+
+    let SQL = "INSERT INTO games ( name, cost, category) VALUES (?,?,?)";
+    db.query(SQL,[name, cost, category], (err, result) =>{
         console.log(err);
-        console.log("Connected!");
-    });
-
+        console.log("Dados registrados");
+    })
 });
 
+app.get("/getCards", (req, res) =>{
+    let SQL = "SELECT * from games";
+
+    db.query(SQL, (err, result) =>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+});
 
 app.listen(3001, ()=>{
     console.log("rodando servidor");
