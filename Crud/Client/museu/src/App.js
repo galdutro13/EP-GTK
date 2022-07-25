de23595cf5b755177e6bfedc95608b1feda08c61
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-07-25 19:20:44 -0300

    Adicionando navegação navbar sem routing

--- Crud/Client/museu/src/App.js
+++ Crud/Client/museu/src/App.js
@@ -1,19 +1,37 @@
 import React, {useState, useEffect } from "react"
-import './App.css';
+//import './App.css';
 import Header from "./componentes/Header";
 import Body from "./componentes/Body"; 
 import Footer from "./componentes/Footer";
 import Card from "./componentes/Card";
+import Navbar from "./componentes/navbar";
+import Artista from "./componentes/Artista";
 
 function App() {
-
+  let component
+  switch(window.location.pathname){
+    case "/":
+      component = <Footer/>
+      break
+      case "/Body":
+        component = <Body/>
+        break
+        case "/Artista":
+          component = <Artista/>
+          break
+        case "/Relatorios":
+          component = <Header/>
+          break
+  }
+  
   return(
     <>
+      <Navbar/>
+      {component}
       <Header/>
-      <Body />
-      <Card/>
       <Footer />
     </>
+    
   )
 }
   
