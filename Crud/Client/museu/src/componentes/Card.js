15f30a4367f291eeba31d33d89d3de78dfde8a64
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-07-25 19:23:11 -0300

    Criando cards para mostra das informações

--- Crud/Client/museu/src/componentes/Card.js
+++ Crud/Client/museu/src/componentes/Card.js
@@ -1,15 +1,18 @@
 import React, {useState, useEffect } from "react"
 import Axios from "axios";
-
+import "./card.css"
 
 export default function Card(props){
 
     return(
         <div className="cardContainer">
-            <h1 className="cardTitle">{props.name}</h1>
-            <p className="cardCost">{props.cost}</p>
-            <p className="cardCategory">{props.category}</p>
-            
+            <h1 className="cardTitle">Objetos</h1>
+            <p className="cardArtista">{props.artista}</p>
+            <p className="cardAno">{props.ano}</p>
+            <p className="cardPeriodo">{props.periodo}</p>
+            <p className="cardTitulo">{props.titulo}</p>
+            <p className="cardPais">{props.pais}</p>
+            <p className="cardDescricao">{props.descricao}</p>
         </div>
         
     )
