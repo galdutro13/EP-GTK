0c1847c58a9d1ae029c7e93f1a6226558eeabb06
Mateus Santos Saldanha <Mateusaldanha@usp.br>
2022-07-27 12:27:18 -0300

    Trocando div por table e estilizando

--- Crud/Client/museu/src/componentes/Card.js
+++ Crud/Client/museu/src/componentes/Card.js
@@ -2,15 +2,24 @@ import React from "react"
 import "./card.css"
 export default function Card(props){
     return(
-        <div className="Centralizador">
-            <h1 className="cardTitle">Objetos</h1>
-            <p className="cardArtista">Artista: {props.artista}</p>        
-            <p className="cardAno">Ano de criação: {props.ano}</p>
-            <p className="cardPeriodo">Periodo:{props.periodo}</p>
-            <p className="cardTitulo">Nome da Obra: {props.titulo}</p>
-            <p className="cardPais">Pais:{props.pais}</p>
-            <p className="cardDescricao">Descrição: {props.descricao}</p>
-        </div>
+            <table border="1" cellpadding="5px" cellspacing="0" id="tabela">
+                <tr>
+                    <th className="celula">Nome do Artista</th>
+                    <th className="celula">Ano de criação</th>
+                    <th className="celula">Periodo</th>
+                    <th className="celula">Nome da Obra</th>
+                    <th className="celula">Pais</th>
+                    <th className="celula">Descrição</th>
+                </tr>
+                <tr>
+                    <td className="celula">{props.artista}</td>
+                    <td className="celula">{props.ano}</td>
+                    <td className="celula">{props.periodo}</td>
+                    <td className="celula">{props.titulo}</td>
+                    <td className="celula">{props.pais}</td>
+                    <td className="celula">{props.descricao}</td>
+                </tr>
+            </table>
        
     )
 }
